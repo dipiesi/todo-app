f45bb7d0f541815b0e2fe4896503d3a001b431d0
“dipiesi” <davipsales@gmail.com>
2025-01-05 11:24:01 -0300

    Correct ITEMS import bug.

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -3,38 +3,38 @@ import { useState } from "react";
 import PaginationButtons from "./PaginationButtons";
 import TodoList from "./TodoList";
 import BottomBar from "./BottomBar.jsx";
-import ITEMS from "../data.js";
+import DATA from "../data.js";
 
-let items = ITEMS;
+let ITEMS = [...DATA];
 
 export default function TodoCard() {
   const [currentPage, setCurrentPage] = useState("not-done");
   const [todoList, setTodoList] = useState(
-    items.filter((item) => !item.completed)
+    ITEMS.filter((item) => !item.completed)
   );
 
   function handlePageChange(newPage) {
     setCurrentPage(newPage);
     if (newPage === "not-done") {
-      setTodoList(items.filter((item) => !item.completed));
+      setTodoList(ITEMS.filter((item) => !item.completed));
     } else if (newPage === "done") {
-      setTodoList(items.filter((item) => item.completed));
+      setTodoList(ITEMS.filter((item) => item.completed));
     }
   }
 
   function handleItemStateChange(itemId) {
-    const item = items.find((item) => item.id === itemId);
+    const item = ITEMS.find((item) => item.id === itemId);
     item.completed = !item.completed;
 
     if (currentPage === "not-done") {
-      setTodoList(items.filter((item) => !item.completed));
+      setTodoList(ITEMS.filter((item) => !item.completed));
     } else if (currentPage === "done") {
-      setTodoList(items.filter((item) => item.completed));
+      setTodoList(ITEMS.filter((item) => item.completed));
     }
   }
 
   function handleItemDeletion(itemId) {
-    items = items.filter((item) => item.id !== itemId);
+    ITEMS = ITEMS.filter((item) => item.id !== itemId);
     setTodoList((prevList) => prevList.filter((item) => item.id !== itemId));
   }
 
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
-const ITEMS = [
+const DATA = [
   { id: 1, todo: "This is a list item", completed: false },
   { id: 2, todo: "This is a completed item", completed: true },
 ];
 
-export default ITEMS;
+export default DATA;
